3b31e2201e5591d33eb6056ab66bf9f1a553b05a
thompsond50263-svg <thompsond50263@masseyhigh.school.nz>
2026-04-20 10:21:38 +1200

    working integer calculator

---
+++ C_07_integer_calc.py
@@ -0,0 +1,45 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+def int_check(question, low):
+    error = f"Please enter a number that is more than {low}\n"
+
+    while True:
+        try:
+            # ask the user for a number
+            response = int(input(question))
+
+            # check that the number is more than or equal to low
+            if response >= low:
+                return response
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+
+
+# calculates how many bits are needed to represent an integer
+def integer_calc():
+    # Ask the user to enter an integer (more than / equal to 0)
+    integer = int_check(question="Integer: ", low=0)
+
+    # convert the integer to binary and work out the number of bits needed
+    raw_binary = bin(integer)
+
+    # remove the leading '0b' from the raw binary conversion
+    binary = raw_binary[2:]
+    num_bits = len(binary)
+
+    # Set up answer and return it
+    answer = f"{integer} in binary is {binary}.  We ned {num_bits} to represent it."
+
+    return answer
+
+
+# Main routine goes here
+integer_ans = integer_calc()
+print(integer_ans)
+
+
+
+
