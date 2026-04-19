6dee50531b5a221b2ee31fa447a129f463c8ffa5
thompsond50263-svg <thompsond50263@masseyhigh.school.nz>
2026-04-20 10:20:58 +1200

    working text bits

---
+++ C_05_text_bits.py
@@ -0,0 +1,20 @@
+# Calculates number of bits needed to represent in ascii
+def calc_text_bits():
+
+    # Get text from user
+    response = input("Enter some text: ")
+
+    # Calculate bits needed
+    num_chars = len(response)
+    num_bits = num_chars * 8
+
+    # Set up answer and return it
+    answer = (f"{response} has {num_chars} characters.  "
+              f"\nWe need {num_chars} x 8 bits to represent it "
+              f"\nwhich is {num_bits} bits.")
+
+    return answer
+
+# Main Routine goes here
+text_ans = calc_text_bits()
+print(text_ans)
